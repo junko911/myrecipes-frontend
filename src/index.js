bf039a02cdc5fb139a61be1313ea8758a50d85b2
cathjime <xcatherine.jimenez@gmail.com>
2020-09-06 11:19:59 -0400

    cuisine-bar-and-buttons-done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 document.addEventListener("DOMContentLoaded", function(e){
     const cuisineContainer = document.querySelector(".container")
     const ingredientsForm = document.querySelector(".form-inline")
+    const ingredientsFormInput = document.querySelector(".form-control")
     
     cuisineContainer.addEventListener("click", function(e){
         if(e.target.className === "col"){
-            //grab textContent of button clicked
+            let cuisineType = e.target.textContent
+            // get cuisine type to append to new page
+            
             ingredientsForm.addEventListener("submit", function(e){
                 e.preventDefault()
+
+                let formInput = ingredientsFormInput.value
+                //use form input to filter and bring up recipes by ingredient
+
+                cuisineContainer.innerHTML = 
+                `
+                <div id="second-page">
+                    <div class="cuisine-bar">${cuisineType}</div>
+                    <button id="nuts">Dairy Free</button>
+                    <button id="eggs">Egg Free</button>
+                    <button id="nuts">Nut Free</button>
+                    <button id="eggs">Peanut Free</button>
+                    <button id="nuts">Shellfish Free</button>
+                    <button id="eggs">Wheat Free</button>
+                    <button id="nuts">Soy Free</button>
+                    <button id="eggs">Fish Free</button>
+                </div>
+                `
             })
             
         }
