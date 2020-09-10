d4e518e5a5a286a37d91716b688d57f14a5325cc
cathjime <xcatherine.jimenez@gmail.com>
2020-09-09 20:19:20 -0400

    FUNCTIONAL COMMENTS

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function(e){
     const ingredientsFormInput = document.querySelector(".form-control")
     const apiUrl = "http://localhost:3000/api/v1/cuisines"
     const commentsUrl = "http://localhost:3000/api/v1/comments"
-    let recipeContent
     let filterKeywords = []
     let cuisineType = ""
     let ingredient = ""
@@ -52,7 +51,6 @@ document.addEventListener("DOMContentLoaded", function(e){
                     .then(resp => resp.json())
                     .then(data => {
                         allRecipesArray = data 
-                        console.log(allRecipesArray)
                         renderRecipes(recipeContainer, data)})  
                 }
                 fetchFilteredRecipes()
@@ -72,6 +70,26 @@ document.addEventListener("DOMContentLoaded", function(e){
                 
                     }
                 })
+
+
+                //********
+                const recipesContainer = cuisineContainer.children[0].children[1]
+                
+                recipesContainer.addEventListener("submit", function(e){
+                    e.preventDefault()
+                    console.log(e.target.id)
+                    let newComment = e.target.children[0].value
+                    console.log(newComment)
+                    let newCommentLi = document.createElement("li")
+                    newCommentLi.textContent = newComment
+                    console.log(newCommentLi)
+                    let recipeId = e.target.id
+
+                    let comments = e.target.previousElementSibling
+                    console.log(comments)
+                    comments.append(newCommentLi)
+                })
+                //
                 
 
                
@@ -90,7 +108,7 @@ document.addEventListener("DOMContentLoaded", function(e){
         }        
     })
     
-//                 //render all recipes
+              //render all recipes
     const renderRecipes = (container, recipesArray) => {
         document.querySelector('.recipe-container').innerHTML = ""
 
@@ -98,11 +116,11 @@ document.addEventListener("DOMContentLoaded", function(e){
             renderRecipe(container, recipe)
         })
     }
-//                 //render single recipe
+                //render single recipe
     const renderRecipe = (container, recipe) => {
         let recipeId = recipe.id
-        console.log(recipe.content)
         let recipeDiv = document.createElement("div")
+        recipeDiv.dataset.id = recipe.id
         recipeDiv.className = "filtered-recipes"
         recipeDiv.innerHTML = 
         `
@@ -111,11 +129,9 @@ document.addEventListener("DOMContentLoaded", function(e){
         <button class="like-btn">Like ❤️</button>
         <button class="recipe-detail-btn" data-id=${recipeId}>See Detail</button> 
         <div class="recipe-detail" id=${recipeId} style="display: none;"> ${recipe.content} </div>
-        <ul class="comments">
-        <li>**USER COMMENT 1**</li>
-        <li>**USER COMMENT 2**</li>
+        <ul class="comments" id=${recipe.id}>
         </ul>
-        <form class="comment-form">
+        <form class="comment-form" id=${recipe.id}>
             <input class="comment-input" type="text" name="comment" placeholder="Add a comment..."/>
             <button class="comment-button" type="submit">Add Comment</button>
         </form>
@@ -139,44 +155,26 @@ document.addEventListener("DOMContentLoaded", function(e){
 })
 
 
-
- /*
-//                     const recipeDetailButton = document.querySelector(".recipe-detail-btn")
-//                     const recipeDetails = document.querySelector(".recipe-detail")
-//                     const commentForm = document.querySelector(".comment-form")
-//                     // const commentButton = document.querySelector(".comment-button")
-//                     recipeDetailButton.addEventListener("click", function(e){
-//                     recipeDropdown = !recipeDropdown
-//                     if (recipeDropdown) {
-//                         recipeDetails.style.display = "block";
-//                         recipeDetailButton.textContent = "Less Detail"
-//                       } else {
-//                         recipeDetails.style.display = "none";
-//                         recipeDetailButton.textContent = "See Detail"
-//                       }
-//                     })
-//                 */
-
 //add comments to database 
 
-//                 // commentForm.addEventListener("submit", function(e){
-//                 //     e.preventDefault()
-//                 //     let newComment = document.querySelector(".comment-input").value
+                // commentForm.addEventListener("submit", function(e){
+                //     e.preventDefault()
+                //     let newComment = document.querySelector(".comment-input").value
 
-//                 //     // fetch(commentsUrl, {
-//                 //     //     method: "POST",
-//                 //     //     headers:{
-//                 //     //         "Content-Type": "application.json",
-//                 //     //         "Accepts": "application/json"
-//                 //     //     },
-//                 //     //     body: JSON.stringify({
-//                 //     //         comments: newComment
-//                 //     //     })
-//                 //     // })
-//                 //     // .then(resp => resp.json())
-//                 //     // .then(recipe => renderRecipe(recipe))
-//                 //       
-//                 // })
+                //     // fetch(commentsUrl, {
+                //     //     method: "POST",
+                //     //     headers:{
+                //     //         "Content-Type": "application.json",
+                //     //         "Accepts": "application/json"
+                //     //     },
+                //     //     body: JSON.stringify({
+                //     //         comments: newComment
+                //     //     })
+                //     // })
+                //     // .then(resp => resp.json())
+                //     // .then(recipe => renderRecipe(recipe))
+                      
+                // })
 
 
 
